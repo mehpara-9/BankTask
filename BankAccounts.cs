a230de05de187f57133d790b8b1a24019860fed6
mehpara-9 <mehparefk-bb208@code.edu.az>
2025-03-25 16:38:14 +0400

    BankTask

---
+++ BankAccounts.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankTask
+{
+    public class BankAccounts
+    {
+        private decimal _balanc;
+
+        public decimal _balancbalace
+        {
+            get { return _balanc; }
+
+        }
+
+        private int _AccountNumber;
+        public int AccountNumber
+        {
+            get { return _AccountNumber; }
+
+        }
+        public string Ownername { get; set; }
+
+        public BankAccounts(int AAccountNumber, string customername, int balanc)
+        {
+            _AccountNumber = AAccountNumber;
+            Ownername = customername;
+            _balanc = balanc;
+
+        }
+        public void Deposit(decimal depositamount)
+        {
+            if (depositamount > 0)
+            {
+                _balanc += depositamount;
+                Console.WriteLine($"{depositamount}azn ugurla kocuruldu,cari balans {_balanc}");
+
+            }
+            else
+            {
+                Console.WriteLine("elave edeceyim mebleg menfi ola bilmez");
+            }
+        }
+        public void WithDraw(decimal withdrawamount)
+        {
+            if (_balanc > withdrawamount)
+            {
+                _balanc -= withdrawamount;
+                Console.WriteLine($"{withdrawamount} cixarildi");
+            }
+        }
+        public void DisplayAccountInfo()
+        {
+            Console.WriteLine($"balansiniz {_balanc}, adiniz{Ownername},musteri nomreniz{AccountNumber}");
+        }
+
+
+        public void TransferFunds(BankAccounts recipient, decimal amount)
+        {
+            recipient.Deposit(amount);
+            Console.WriteLine($"{recipient} sexsine {amount} azn qeder mebleg kocuruldu");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿namespace BankTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccounts accounts = new BankAccounts(123, "jasd", 123);
+            BankAccounts account1 = new BankAccounts(123, "jasd", 123);
+            Console.WriteLine("istediyiniz emeliyyati secin");
+            Console.WriteLine("\n 1.DisplayAccountInfo \n 2. Deposit \n 3 WithDraw  \n 4 esas menyuya qayit \n 6 TransferFunds \n 0 Quit Application");
+            int Menu = int.Parse(Console.ReadLine());
+
+            switch (Menu)
+            {
+                case 1:
+                    account1.DisplayAccountInfo();
+                    break;
+                case 2:
+                    Console.WriteLine("ne qeder pul elave etmek isteyirsen?");
+                    decimal deposit = int.Parse(Console.ReadLine());
+                    account1.Deposit(deposit);
+                    break;
+                case 3:
+                    Console.WriteLine("ne qeder pul elave etmek isteyirsen?");
+                    decimal withdraw = int.Parse(Console.ReadLine());
+                    account1.WithDraw(withdraw);
+                    break;
+                case 4:
+                    Console.WriteLine("esas menyuya qayit");
+                    break;
+                case 0:
+                    Console.WriteLine("sonlandir");
+                    return;
+
+
+            }
+        }
+    }
+}
